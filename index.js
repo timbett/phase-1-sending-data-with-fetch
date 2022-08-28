f15aabe31b6dcd16c004e1932d44552b3dbea225
Timothy <tkkibet13@gmail.com>
2022-08-28 20:36:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+     method: "POST",
+     headers: {
+       "Content-Type": "application/json",
+       Accept: "application/json",
+     },
+     body: JSON.stringify({
+       name: name,
+       email: email,
+     }),
+   })
+     .then((response) => response.json())
+     .then((data) => {
+       console.log(data);
+       document.body.innerHTML += data.id;
+     })
+     .catch((err) => {
+       console.log(err);
+       document.body.innerHTML += err.message;
+     });
+ }
+ submitData('steve', 'user@gmail.com')
